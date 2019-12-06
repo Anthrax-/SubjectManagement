0be7d97fc9610d9390fd39b14b4773f8303a3c50
Mahmoud Mostafa <hoda002009@hotmail.com>
2019-12-06 18:56:06 +0200

    Adding Admin class

---
+++ SubjectMangagement/SubjectManagement/Admin.cpp
@@ -0,0 +1,32 @@
+#include "Admin.h"
+void Admin::Login()
+{
+}
+
+void Admin::AddNewStudent()
+{
+}
+
+void Admin::AddNewCourse()
+{
+}
+
+
+void Admin::EnterCoursePrerequisite()
+{
+}
+
+
+void Admin::VLOASiSC()
+{
+}
+
+
+void Admin::VLOACOSS()
+{
+}
+
+
+void Admin::EACD()
+{
+}
---
+++ SubjectMangagement/SubjectManagement/Admin.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <string>
+#include <iostream>
+using namespace std;
+class Admin
+{
+	string Name;
+	string Password;
+public:
+	void Login();
+	void AddNewStudent();
+	void AddNewCourse();
+	void EnterCoursePrerequisite();
+	void VLOASiSC();
+	void VLOACOSS();
+	void EACD();
+};
+
+
