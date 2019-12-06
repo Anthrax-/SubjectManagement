f58d683c94035d0498507d242ce5f3b94b8406e9
Mahmoud Mostafa <hoda002009@hotmail.com>
2019-12-06 15:37:27 +0200

    Added CSVFile CLass

--- SubjectMangagement/SubjectManagement/CSVFile.cpp
+++ SubjectMangagement/SubjectManagement/CSVFile.cpp
@@ -4,3 +4,18 @@ CSVFile::CSVFile(string path)
 	Path = path;
 	Stream.open(Path);
 }
+CSVFile::~CSVFile()
+{
+	Stream.close();
+}
+vector<string> CSVFile::Load()
+{
+	vector<string> result;
+	Stream.seekg(0);
+	string line;
+	while (getline(Stream, line))
+	{
+		result.push_back(line);
+	}
+	return result;
+}
--- SubjectMangagement/SubjectManagement/CSVFile.h
+++ SubjectMangagement/SubjectManagement/CSVFile.h
@@ -1,11 +1,14 @@
 #include <string>
 #include <fstream>
+#include <vector>
 #pragma once
 using namespace std;
 class CSVFile
 {
 	public:
 	CSVFile(string path);
+	~CSVFile();
+	vector<string> Load();
 	private:
 	fstream Stream;
 	string Path;
