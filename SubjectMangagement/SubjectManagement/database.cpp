b6fe2fe1dcb79cf0dbc8f2b315436a84d4d91794
Mahmoud Mostafa <hoda002009@hotmail.com>
2019-12-07 15:32:32 +0200

    assigned tasks to persons

--- SubjectMangagement/SubjectManagement/database.cpp
+++ SubjectMangagement/SubjectManagement/database.cpp
@@ -10,23 +10,23 @@ void Database::load() {
 	Database::Users = User::LoadUsers();
 
 }
-Course Database::GetCourse(string courseID)
+Course Database::GetCourse(string courseID) //farah
 {
 	//this function should loop through courses vector and return the course with the courseid.
 }
 
-vector<User> Database::GetUser(int userID)
+vector<User> Database::GetUser(int userID)//samir
 {
 	//this function should return user with the id in parameters
 }
 
-vector<User> Database::GetUsers(int Role)
+vector<User> Database::GetUsers(int Role)//samir
 {
 		//this function should return all the users with the role in parameters.
 
 }
 
-vector<Course> Database::GetPreRequiredCourses(Course course)
+vector<Course> Database::GetPreRequiredCourses(Course course)//farah
 {
 	//this function should take a course return a vector that countains all the pre-required courses 
 	//it should use the GetCourse Function
