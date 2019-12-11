a5c6ef961e5af80b980f1de119cad50e41e47831
youssef gamal <youssef2018170473@cis.asu.edu.eg>
2019-12-11 20:05:05 +0200

    1111

--- SubjectMangagement/SubjectManagement/Course.cpp
+++ SubjectMangagement/SubjectManagement/Course.cpp
@@ -18,13 +18,13 @@ vector<string> Course::GetCoursesLines()
 	vector<string> result;
 	string str = "";
 	for (int i = 0; i < Database::Courses.size(); i++) {
-		str.push_back(Database::Courses[i].Code + ',' + Database::Courses[i].Name + ',' + Database::Courses[i].Hours + ',' + Database::Courses[i].MaxNumOfStudents + ',');
+		str.push_back(Database::Courses[i].Code + ',' + Database::Courses[i].Name +','+ Database::Courses[i].Hours+',' + Database::Courses[i].MaxNumOfStudents + ',');
 		for (int j = 0; j < Database::Courses[i].PreRequiredCourses.size(); j++) {
 			str.push_back(Database::Courses[i].PreRequiredCourses[j] + ',');
 		}str.pop_back();
 		result.push_back(str);
 	
-	}return result;
+	}
 	
 return result;
 }
