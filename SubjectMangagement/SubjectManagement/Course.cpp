613a48ebd7d0db6dece523ceead176130ad66ce4
Mahmoud Mostafa <hoda002009@hotmail.com>
2019-12-13 00:47:21 +0200

    Used Pointers Everywhere

--- SubjectMangagement/SubjectManagement/Course.cpp
+++ SubjectMangagement/SubjectManagement/Course.cpp
@@ -2,13 +2,13 @@
 #include "CSVFile.h"
 #include "Database.h"
 
-vector<Student> Course::GetStudents()
+vector<Student*> Course::GetStudents()
 {
-	vector<Student> result;
+	vector<Student*> result;
 	for (int i = 0; i < Database::Students.size(); i++)
 	{
-		Student s = Database::Students[i];
-		if (s.HaveCourseInProgress(Code))
+		Student* s = Database::Students[i];
+		if (s->HaveCourseInProgress(Code))
 			result.push_back(s);
 	}
 	return result;
@@ -18,9 +18,9 @@ vector<string> Course::GetCoursesLines()
 	vector<string> result;
 	string str = "";
 	for (int i = 0; i < Database::Courses.size(); i++) {
-		str.push_back(Database::Courses[i].Code + ',' + Database::Courses[i].Name +','+ Database::Courses[i].Hours+',' + Database::Courses[i].MaxNumOfStudents + ',');
-		for (int j = 0; j < Database::Courses[i].PreRequiredCourses.size(); j++) {
-			str.push_back(Database::Courses[i].PreRequiredCourses[j] + ',');
+		str+=Database::Courses[i]->Code + "," + Database::Courses[i]->Name +","+ to_string(Database::Courses[i]->Hours)+"," + to_string(Database::Courses[i]->MaxNumOfStudents) + ",";
+		for (int j = 0; j < Database::Courses[i]->PreRequiredCourses.size(); j++) {
+			str+=Database::Courses[i]->PreRequiredCourses[j] + ",";
 		}str.pop_back();
 		result.push_back(str);
 	
@@ -28,23 +28,23 @@ vector<string> Course::GetCoursesLines()
 	
 return result;
 }
-vector<Course> Course::LoadCourses()
+vector<Course*> Course::LoadCourses()
 {
-	vector<Course> Result;
+	vector<Course*> Result;
 	CSVFile CoursesFile("courses.csv");
 	auto lines = CoursesFile.Load();
 	for (auto line : lines)
 	{
 		auto parsedLine = CSVFile::ParseLine(line);
-		Course course;
-		course.Code = parsedLine[0];
-		course.Name = parsedLine[1];
-		course.Hours = stoi(parsedLine[2]);
-		course.MaxNumOfStudents = stoi(parsedLine[3]);
+		Course* course = new Course();
+		course->Code = parsedLine[0];
+		course->Name = parsedLine[1];
+		course->Hours = stoi(parsedLine[2]);
+		course->MaxNumOfStudents = stoi(parsedLine[3]);
 		vector<string> PreCourses;
 		for (int i = 4; i < parsedLine.size(); i++)
 			PreCourses.push_back(parsedLine[i]);
-		course.PreRequiredCourses = PreCourses;
+		course->PreRequiredCourses = PreCourses;
 		Result.push_back(course);
 	}
 	return Result;
